08aedcc18c278588ff1d6ebca111dbd924aef773
JDS <jdorber94@gmail.com>
2026-04-03 13:13:30 +0200

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GuildProtocolDashboard from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GuildProtocolDashboard />
+  </React.StrictMode>,
+)
